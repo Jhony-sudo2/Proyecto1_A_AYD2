a61f501ff7fa2eb9f2d0932e2f300151dcefc188
jhony <jhony.fuentes19@gmail.com>
2026-02-16 21:56:21 -0600

    backend

---
+++ back/src/main/java/com/ayd2/congress/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.ayd2.congress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
